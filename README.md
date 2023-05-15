a41615a351d5370b7d3ae056617d226d68ef413c
ivanapizzo <ivb.pizzo@gmail.com>
2023-05-15 14:06:09 +0200

    todo dia1

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 
 Assets ---> carpeta para fotos y media
 
+
 Archivos app ---> componentes principales
 app.component.html ---> componente raíz
+
+
+href ----> no lo usamos en algular porque hace que "carguen" las cosas. Angular se usa para que no tenga que cargar nada.
--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'formRegister', component: FormRegisterComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
