2aa3e0b46c0bb3a83f18c4e0c961d37e7bcc8104
ivanapizzo <ivb.pizzo@gmail.com>
2023-05-16 12:52:47 +0200

    corrigiendo errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { FormRegisterComponent } from './component/form-register/form-register.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'formRegister', component: FormRegisterComponent}
 
 
 ];
